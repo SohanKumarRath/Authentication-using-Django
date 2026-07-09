d69667eb3cf52668fb26e0592892658c11e0cf8e
SohanKumarRath <sohan.rath9912@gmail.com>
2026-07-09 10:54:08 +0530

    commit this

--- Authentication_project/authentication_app/views.py
+++ Authentication_project/authentication_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import SignupForm
+from django.contrib import messages
 # Create your views here.
 def signupView(req):
     if req.method=='POST':
         form=SignupForm(req.POST)
         if form.is_valid():
             form.save()
+            form=SignupForm()
+            messages.success('Thank you for the signup !!!')
+
     else:
         form=SignupForm()
     return render(req,'signupform.html',{'form':form})
